aa84ea584f3ec6b96ff43f7758bf976752246652
吴寅 <5203017+wuyin246@user.noreply.gitee.com>
2019-12-24 16:59:28 +0800

    11111

--- web/src/store.js
+++ web/src/store.js
@@ -16,7 +16,7 @@ export default new Vuex.Store({
     },
     actions: {
         // Action 提交的是 mutation，而不是直接变更状态。
-        // Action 可以包含任意异步操作。
+        // Action 可以包含任意异步操作。dwadwad
         increment(context) {
             context.commit('increment')
         }
